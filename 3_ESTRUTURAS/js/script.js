2abea8e3bda2f0a57706854171dbbf4cf9448353
AlexsandroAN <alexaraujony@gmail.com>
2023-09-26 08:19:49 -0300

    32. S3E7 - Função prompt

---
+++ 3_ESTRUTURAS/js/script.js
@@ -0,0 +1,43 @@
+// 1 - variáveis
+
+let nome = "Alex";
+
+console.log(nome);
+
+nome = "Alexsandro";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 mais sobre variáveis
+
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10,
+    b = 20,
+    c = 30;
+
+console.log(a, b, c)
+
+const nomecompleto = "Alexsandro"
+const nomeCompleto = "Alexsandro Araújo"
+
+console.log(nomecompleto)
+console.log(nomeCompleto)
+let _teste = "ok"
+let $teste = "ok"
+
+console.log(_teste, $teste)
+
+// 3 - prompt
+const age = prompt("Digite a sua idade:");
+console.log(`Voçe tem ${age} anos.`);
